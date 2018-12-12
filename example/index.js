b1586f9d796e69b3ca28ab521a02ca34615e9aac
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-12-12 12:03:44 +0100

    added node-visalist

--- example/index.js
+++ example/index.js
@@ -1,26 +1,62 @@
 const WorldPackers = require('..');
 const world = new WorldPackers();
 
+const Rome2Rio = require('node-rome2rio');
+const rome = new Rome2Rio({
+  apiKey: 'adAxxVif'
+});
+
+const slugify = require('slugify');
+const Visalist = require('node-visalist');
+const visa = new Visalist({
+  apiKey: 'x4xhhAeK6MajeqcB'
+});
+
+const DEPARTURE = 'Paris,France';
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 async function init() {
   try {
-    // Find web development trips in Brazil
-    const places = await world.getPlaces('south_america', 'brazil');
-    /*
-    // Find web development trips in Europe
     const places = await world.getPlaces('europe');
-    
-    // Find web development trips in the world
-    const places = await world.getPlaces();
-
-    // Find all regions where there are web development trips
-    const regions = await world.getRegions()
-
-    // Find all countries in South American where there are web development trips
-    const countries = await world.getCountries('south_america')
-    */
-    console.log(places);
+    const travelArray = [];
+    await asyncForEach(places, async place => {
+      try {
+        const travel = await rome.search({
+          departure: DEPARTURE,
+          arrival: `${place.location.city},${place.location.country}`
+        });
+        const route = travel.routes[0];
+        const visaRequirements = await visa.getRequirementsForACountry(
+          'france',
+          slugify(place.location.country, {
+            lower: true
+          })
+        );
+        const item = {
+          departure: DEPARTURE,
+          arrival: `${place.location.city}, ${place.location.country}`,
+          duration: route.totalDuration,
+          distance: route.distance,
+          price: route.indicativePrices[0].price,
+          visa: visaRequirements,
+          ...place
+        };
+        travelArray.push(item);
+      } catch (err) {
+        //console.log('\n');
+      }
+    });
+    const filteredArray = travelArray.filter(
+      item => item.details.sleepType === 'Private Room'
+    );
+    console.log(filteredArray.sort((a, b) => (a.price < b.price ? -1 : 1)));
   } catch (err) {
-    console.log(err);
+    //console.log(err);
   }
 }
 
